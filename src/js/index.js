77595dd35c3e3b8a5ba68e35280682d7650e4110
kaileS2 <159574188+kaileS2@users.noreply.github.com>
2024-03-09 15:54:13 -0300

    feat: Bug de interface resolvido
    - O bug de a div box-projetos só receber doi clicks foi resolvido

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,20 @@
 var box_projetos = document.querySelector('div.box-projetos')
 var text_projetos = document.querySelector("p.text-projetos")
 
-box_projetos.addEventListener("click", function() { 
-    box_projetos.style.width = "500px"
-    box_projetos.style.height = "300px"
+var isExpanded = false; // Variável de controle para alternar entre os dois estados
 
-    text_projetos.hidden = true
-
-    box_projetos.addEventListener("click", function() {
-        box_projetos.style.width = "100px"
-        box_projetos.style.height = "100px"
-
-        text_projetos.hidden = false
-    })
-
-});
-
-text_projetos.addEventListener("click", function() { 
-    box_projetos.style.width = "500px"
-    box_projetos.style.height = "300px"
-
-    text_projetos.hidden = true
-
-    box_projetos.addEventListener("click", function() {
-        box_projetos.style.width = "100px"
-        box_projetos.style.height = "100px"
-
-        text_projetos.hidden = false
-    })
-
-});
-
+function aumentarEDiminuirBoxProjetos() { 
+    if (!isExpanded) {
+        box_projetos.style.width = "500px";
+        box_projetos.style.height = "300px";
+        text_projetos.hidden = true;
+        isExpanded = true;
+    } else {
+        box_projetos.style.width = "100px";
+        box_projetos.style.height = "100px";
+        text_projetos.hidden = false;
+        isExpanded = false;
+    }
+};
 
 document.addEventListener("DOMContentLoaded", load, false);
